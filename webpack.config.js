4e304a9284830d9196c22b6cf9b62e0f21098b9c
Kisha Prudente <kisha.prudente@gmail.com>
2020-05-22 16:45:48 -0400

    configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const config = {
 	// Update the entry point
-	entry: path.join("./public/assets/js/app.js"),
+	entry: path.join(__dirname + "/public/index.js"),
 	output: {
 		// Set the path and filename for the output bundle (hint: You will need to use "__dirname")
 		path: path.join(__dirname + "/public/dist"),
@@ -14,12 +14,15 @@ const config = {
 		new WebpackPwaManifest({
 			name: "Budget Tracker App",
 			short_name: "Budget Tracker App",
+			filename: "manifest.webmanifest",
 			description: "My awesome Progressive Web App!",
 			background_color: "#ffffff",
-			crossorigin: "null", //can be null, use-credentials or anonymous
+			display: "standalone",
+			start_url: "/",
+			fingerprints: false,
 			icons: [
 				{
-					src: path.resolve("./public/icons/icon-192x192.png"),
+					src: path.resolve("./public/assets/icons/icon-192x192.png"),
 					sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
 				},
 			],
