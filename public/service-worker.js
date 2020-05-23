80d5ceffda529d68813792115313f29a87eb9e66
Kisha Prudente <kisha.prudente@gmail.com>
2020-05-22 21:11:41 -0400

    edited service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,11 @@
-const FILES_TO_CACHE = ["/", "/index.html"];
+const FILES_TO_CACHE = [
+	"/",
+	"/index.html",
+	"/dist/manifest.webmanifest",
+	"/assets/styles.css",
+	"/assets/icons/icon-192x192.png",
+	"/assets/icons/icon-512x512.png",
+];
 
 const STATIC_CACHE = "static-cache-v1";
 const RUNTIME_CACHE = "runtime-cache";
@@ -37,16 +44,16 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
 	// non GET requests are not cached and requests to other origins are not cached
-	if (
-		event.request.method !== "GET" ||
-		!event.request.url.startsWith(self.location.origin)
-	) {
-		event.respondWith(fetch(event.request));
-		return;
-	}
+	// if (
+	// 	event.request.method !== "GET" ||
+	// 	!event.request.url.startsWith(self.location.origin)
+	// ) {
+	// 	event.respondWith(fetch(event.request));
+	// 	return;
+	// }
 
 	// handle runtime GET requests for data from /api routes
-	if (event.request.url.includes("/")) {
+	if (event.request.url.includes("/api/transaction")) {
 		// make network request and fallback to cache if network request fails (offline)
 		event.respondWith(
 			caches.open(RUNTIME_CACHE).then((cache) => {
